ccb0d8ed374c5a95b05af293bac346a83ec1f022
Oskari Pölhö <osku@oskaripolho.com>
2022-09-08 13:49:15 +0300

    add elevator buttons

--- src/components/Table/Row.tsx
+++ src/components/Table/Row.tsx
@@ -2,6 +2,7 @@ import React, {useContext} from 'react';
 import {
   Flex,
   IconButton,
+  Stack
 } from '@contentful/f36-components';
 import * as icons from '@contentful/f36-icons'
 import Cell from './Cell'
@@ -33,7 +34,39 @@ const Row = (props: RowProps) => {
       }
       aria-label={'plus'}
     />
+    <ElevatorButtons rowIndex={rowIndex} setRows={setRows} rows={rows}/>
   </Flex>
 }
 
 export default Row;
+
+interface ElevatorProps {
+  rowIndex: number | undefined,
+  rows: RowProps[]
+  setRows: () => void
+}
+
+const moveRowUpAndDown = (rows: any, rowIndex: number|undefined, direction: -1 | 1, setRows: (arg: RowProps[]) => void) => {
+  if (rowIndex === undefined) return
+  const newRows = [...rows]
+  const newRow = newRows.splice(rowIndex, 1);
+  newRows.splice(rowIndex + direction, 0, newRow[0])
+  setRows(newRows)
+}
+
+const ElevatorButtons = ({rowIndex, setRows, rows}: ElevatorProps) => {
+  return (
+    <Stack flexDirection='column'>
+    <IconButton
+      icon={<icons.ArrowUpIcon/>}
+      onClick={() => {moveRowUpAndDown(rows, rowIndex, -1, setRows)}}
+      aria-label={'up'}
+    />
+    <IconButton
+      icon={<icons.ArrowDownIcon/>}
+      onClick={() => {moveRowUpAndDown(rows, rowIndex, 1, setRows)}}
+      aria-label={'down'}
+    />
+    </Stack>
+  )
+}
